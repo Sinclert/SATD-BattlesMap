034ced572b7608086f23c9b061d8e684e4b69091
Silvia <100316961@alumnos.uc3m.es>
2017-12-09 16:50:01 +0100

    The map is alive :+1:

--- test.js
+++ test.js
@@ -1,13 +1,44 @@
+//import L from 'leaflet'
 //Global variables
 
 var fileData = null;
 var data = [];
-var colors = {Lannister: "#cdd10a", Stark: "#9ddce8", Greyjoy: "#999d9e", Bolton: "#ce4021", 
+var colors = {Lannister: "#cdd10a", Stark: "#9ddce8", Greyjoy: "#999d9e", Bolton: "#ce4021",
              Baratheon: "#6b3410", Darry: "#f25c10", Brotherhood: "#ffffff", Frey: "#327a2d",
              Free: "#c6cec7", Brave: "#84800b", Bracken: "#560505", Karstark: "#10a887",
              Tully: "#fcbfc8", Mallister: "#7c64a8", Night: "#000000", Tyrell: "#ffb7ef",
              Blackwood: "#ff9b9b"};
 
+var map = null;
+
+function create() {
+    map = L.map('map').setView([5,20], 4);
+    mapLink = 
+            '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+    L.tileLayer(
+        'https://cartocdn-ashbu.global.ssl.fastly.net/ramirocartodb/api/v1/map/named/tpl_756aec63_3adb_48b6_9d14_331c6cbc47cf/all/{z}/{x}/{y}.png', {
+        attribution: '&copy; ' + mapLink + ' Contributors',
+        center: [ 5, 20 ],
+        zoom: 4,
+        maxZoom: 8,
+        minZoom: 4,
+        maxBounds: [ [ 50, -30 ], [ -45, 100 ] ]
+        }).addTo(map);
+        
+    // Change the position of the zoom control
+    map.zoomControl.setPosition('bottomright');
+    // Set map boundaries
+    map.setMaxBounds([[50,-30],[-45,100]]);
+    
+    // Get file data
+    readTextFile("./5kings_battles_v1.csv");
+    
+    // Set objects data
+    getBattles(fileData);
+    
+    // Show battles data on the map
+    paintBattles();
+}
 
 function readTextFile(file) {
     var rawFile = new XMLHttpRequest();
@@ -38,23 +69,23 @@ function getBattles(text) {
             attacker_3:         elements[7],
             attacker_4:         elements[8],
             defender_1:         elements[9],
-            defender_2:         elements[12],
-            defender_3:         elements[13],
-            defender_4:         elements[14],
-            attacker_outcome:   elements[15],
-            battle_type:        elements[16],
-            major_death:        elements[17],
-            major_capture:      elements[18],
-            attacker_size:      elements[19],
-            defender_size:      elements[20],
-            attacker_commander: elements[21],
-            defender_commander: elements[22],
-            summer:             elements[23],
-            location:           elements[24],
-            lat:                elements[25],
-            lng:                elements[26],
-            region:             elements[27],
-            note:               elements[28]
+            defender_2:         elements[10],
+            defender_3:         elements[11],
+            defender_4:         elements[12],
+            attacker_outcome:   elements[13],
+            battle_type:        elements[14],
+            major_death:        elements[15],
+            major_capture:      elements[16],
+            attacker_size:      elements[17],
+            defender_size:      elements[18],
+            attacker_commander: elements[19],
+            defender_commander: elements[20],
+            summer:             elements[21],
+            location:           elements[22],
+            lat:                elements[23],
+            lng:                elements[24],
+            region:             elements[25],
+            note:               elements[26]
         };
         data.push(aux);
     }
@@ -62,40 +93,44 @@ function getBattles(text) {
 
 function paintBattles() {
     console.log("in paint battles");
-    data.forEach(function (key) {
-        console.log(key);
+    
+    for (var i = 0; i < data.length - 1; i++) {
+    //data.forEach(function (key) {
+        console.log(data[i]);
         // Radius for circle asignation, multiplied by 10 so that it can be seen
-        var radius = key.attacker_size * 10;
+        var radius = data[i].attacker_size * 10;
         // Color assignation by house
         var color = null;
-        if (key.attacker_1 in colors) {
-            color = colors[key];
+        if (data[i].attacker_1 in colors) {
+            color = colors[data[i].attacker_1];
         } else {
-            var aux = key.attacker_1.split(' ');
-            if (aux[1] in colors) {
-                color = colors[key];
+            var aux = data[i].attacker_1.split(' ');
+            if (aux[0] in colors) {
+                color = colors[aux[0]];
             }
         }
         
         // Attacker circle
-        L.circle([key.lat, key.lng], radius, {
+        L.circle([data[i].lat, data[i].lng], radius, {
                  fillColor: color,
-                 fillOpacity: 0.5
+                 color: color,
+                 fillOpacity: 0.3
                  }).addTo(map);
         
-        if (key.defender_1 in colors) {
-            color = colors[key];
+        if (data[i].defender_1 in colors) {
+            color = colors[data[i].defender_1];
         } else {
-            var aux2 = key.defender_1.split(' ');
-            if (aux2[1] in colors) {
-                color = colors[key];
+            var aux2 = data[i].defender_1.split(' ');
+            if (aux2[0] in colors) {
+                color = colors[aux2[0]];
             }
         }
         
         //Defender circle
-        L.circle([key.lat, key.lng], radius, {
+        L.circle([data[i].lat, data[i].lng], radius, {
                  fillColor: color,
                  fillOpacity: 0.5
                  }).addTo(map);
-    });
+    //});
+    }
 }
